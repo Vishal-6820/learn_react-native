fb5b9449505969487195206a2019f5c8a2ec352e
Vishal Parmar <parmarvishesh97@gmail.com>
2025-05-06 20:28:59 +0530

    Shimmer Effect Bug fix

---
+++ src/screen/Explore.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import ShimmerEffect from './ShimmerEffect';
+
+export default function Explore() {
+  return (
+    <View>
+      <Text>Explore</Text>
+      <ShimmerEffect />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
